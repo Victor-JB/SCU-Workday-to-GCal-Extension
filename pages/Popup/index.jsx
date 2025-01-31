0661870c82d71af7dd0c055e77ff8824078aad60
Victor Joulin-Batejat <victorjb2015@gmail.com>
2025-01-31 11:51:11 -0800

    Cleaning up

--- pages/Popup/index.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'solid-js/web';
-
-import Popup from './Popup';
-import './index.css';
-
-render(Popup, document.getElementById('app-container'));
-
-if (module.hot) module.hot.accept();
